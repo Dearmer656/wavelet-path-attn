2b1edf67041b95b510bf128d16f122d58c8e7598
Dearmer656 <sunhongyu008@hotmail.com>
2026-08-26 05:36:13 +0900

    PAT-253: redirect medium hidden_ln rho384 s42 eval to idle elm71

--- examples/pytorch/language-modeling/_gen_medium_hiddenln/test_mix_medium_K1_rho384_hiddenln_s42.sh
+++ examples/pytorch/language-modeling/_gen_medium_hiddenln/test_mix_medium_K1_rho384_hiddenln_s42.sh
@@ -2,7 +2,8 @@
 #SBATCH --job-name=hp2048_medK1_rho384_hiddenln_s42
 #SBATCH --output=/cl/work5/hongyu-s/transformers/examples/pytorch/language-modeling/hotpot_long/logs/%j_medK1_rho384_hiddenln_s42_ckpt15000_hotpot2048.txt
 #SBATCH --partition=gpu_long
-#SBATCH --gres=gpu:a6000:4
+#SBATCH --gres=gpu:6000:4
+#SBATCH --nodelist=elm71
 #SBATCH --exclude=elm66
 #SBATCH --time=24:00:00
 #SBATCH --ntasks=1
