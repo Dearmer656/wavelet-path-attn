ed5a5b0419052a1c3d1c4ff1cbc66b42fee0cb9a
Dearmer656 <sunhongyu008@hotmail.com>
2026-08-25 20:15:13 +0900

    PAT-253: retry K3 L512 s44 eval on a6000 (unpinned) instead of elm72

--- examples/pytorch/language-modeling/_gen_medium_ricker_lenbackfill/test_mix_medium_K3_ricker_128_256_384_s44_L512.sh
+++ examples/pytorch/language-modeling/_gen_medium_ricker_lenbackfill/test_mix_medium_K3_ricker_128_256_384_s44_L512.sh
@@ -2,8 +2,7 @@
 #SBATCH --job-name=hp2048_medK3_ricker_s44
 #SBATCH --output=/cl/work5/hongyu-s/transformers/examples/pytorch/language-modeling/hotpot_long/logs/%j_medK3_ricker_128_256_384_s44_ckpt15000_hotpot2048.txt
 #SBATCH --partition=gpu_long
-#SBATCH --gres=gpu:6000:1
-#SBATCH --nodelist=elm72
+#SBATCH --gres=gpu:a6000:1
 #SBATCH --nodelist=elm54
 #SBATCH --exclude=elm66
 #SBATCH --time=24:00:00
