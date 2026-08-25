20e5976f72fe1e74b53a431e7945d10593ffd5a5
Dearmer656 <sunhongyu008@hotmail.com>
2026-08-25 19:46:05 +0900

    PAT-253: remove stuck elm52 node pin from K3 L512 s42 eval, let scheduler place freely

--- examples/pytorch/language-modeling/_gen_medium_ricker_lenbackfill/test_mix_medium_K3_ricker_128_256_384_s42_L512.sh
+++ examples/pytorch/language-modeling/_gen_medium_ricker_lenbackfill/test_mix_medium_K3_ricker_128_256_384_s42_L512.sh
@@ -3,7 +3,6 @@
 #SBATCH --output=/cl/work5/hongyu-s/transformers/examples/pytorch/language-modeling/hotpot_long/logs/%j_medK3_ricker_128_256_384_s42_ckpt15000_hotpot2048.txt
 #SBATCH --partition=gpu_long
 #SBATCH --gres=gpu:3090:4
-#SBATCH --nodelist=elm52
 #SBATCH --exclude=elm66
 #SBATCH --time=24:00:00
 #SBATCH --ntasks=1
