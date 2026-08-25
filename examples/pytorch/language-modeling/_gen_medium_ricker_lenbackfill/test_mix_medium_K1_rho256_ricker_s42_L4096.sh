d79c212bc1311317e076471737370ec15a4e2977
Dearmer656 <sunhongyu008@hotmail.com>
2026-08-25 19:01:42 +0900

    PAT-253: fix elm26 gres name (q6000 not 6000)

--- examples/pytorch/language-modeling/_gen_medium_ricker_lenbackfill/test_mix_medium_K1_rho256_ricker_s42_L4096.sh
+++ examples/pytorch/language-modeling/_gen_medium_ricker_lenbackfill/test_mix_medium_K1_rho256_ricker_s42_L4096.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=hp2048_medK1_rho256_ricker_s42
 #SBATCH --output=/cl/work5/hongyu-s/transformers/examples/pytorch/language-modeling/hotpot_long/logs/%j_medK1_rho256_ricker_s42_ckpt15000_hotpot2048.txt
 #SBATCH --partition=gpu_long
-#SBATCH --gres=gpu:6000:2
+#SBATCH --gres=gpu:q6000:2
 #SBATCH --nodelist=elm26
 #SBATCH --exclude=elm66
 #SBATCH --time=24:00:00
